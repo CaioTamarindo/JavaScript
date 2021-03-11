420638f75bb72acc9307aa9ee873a6dc26b38b22
CaioTamarindo <70727818+CaioTamarindo@users.noreply.github.com>
2021-03-11 02:26:00 -0300

    Add files via upload
    Projeto em desenvolvimento

---
+++ JavaScript/js/main1.js
@@ -0,0 +1,25 @@
+function bemvindo(){
+    alert("Bem vindo ao meu Quiz sobre Marvel")
+    alert("Clique na pergunta para responder :)")
+}
+
+function comeco(){
+    var resp1= prompt("Digite sua resposta:");
+    if(resp1=="Steve Rogers"){
+        alert("Parabens!!");
+
+        function troca(){
+            document.getElementById("primeira").innerHTML = "Quantas são as joias do infinito?"
+        
+        }
+
+    }else{
+        alert("Voce errou :(");       
+            
+    }
+    }
+
+function segunda(){
+
+}
+
